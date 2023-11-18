8d632818c75a6a5e86123e1e30a03766861d3df6
sarjid <sarjid777@gmail.com>
2023-11-18 11:52:12 +0600

    bkash payment

--- app/Http/Controllers/Payment/BkashPaymentController.php
+++ app/Http/Controllers/Payment/BkashPaymentController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Payment;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\CreatePaymentRequest;
 use Illuminate\Http\Request;
 
